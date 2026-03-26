07995f2e181e81427c3d2ff543fe14549cceb7f8
danieleon08 <dflp2005@gmail.com>
2026-03-26 14:46:34 +0100

    agrego redes sociales y contenido de valores de marca

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -10,15 +10,15 @@ export class ContactComponent {
   // 🌟 Contacto
   contactMethods = [
     { icon: 'fas fa-envelope', title: 'Email', value: 'correo@marckpluss.com' },
-    { icon: 'fas fa-phone', title: 'Teléfono', value: '+57 555 555 5555' },
-    { icon: 'fas fa-map-marker-alt', title: 'Ubicación', value: 'Bogotá, Colombia' },
-    { icon: 'fas fa-calendar', title: 'Disponibilidad', value: 'Lun - Vie, 9AM - 6PM' }
+    { icon: 'fas fa-phone', title: 'Teléfono', value: '+57 320 4795284' },
+    { icon: 'fas fa-map-marker-alt', title: 'Ubicación', value: 'Bogotá, Colombia' }
   ];
 
   socialLinks = [
-    { icon: 'fab fa-whatsapp', url: '', network: 'social-whatsapp' },
-    { icon: 'fab fa-instagram', url: '', network: 'social-instagram' },
-    { icon: 'fab fa-tiktok', url: '', network: 'social-tiktok' },
-    { icon: 'fas fa-at', url: 'mailto:', network: 'social-email' },
+    { icon: 'fab fa-whatsapp', url: 'https://wa.me/573204795284', network: 'social-whatsapp' },
+    { icon: 'fab fa-instagram', url: 'https://www.instagram.com/marckpluss/?hl=es', network: 'social-instagram' },
+    { icon: 'fab fa-tiktok', url: 'https://www.tiktok.com/@inmueblesmarckpluss', network: 'social-tiktok' },
+    { icon: 'fas fa-at', url: 'mailto:correo@marckpluss.com', network: 'social-email-light' },
+    { icon: 'fab fa-facebook', url: 'https://www.facebook.com/profile.php?id=100064359720558&locale=es_ES', network: 'social-email' }
   ];
 }
